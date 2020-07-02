5cd301c6cf912bf72b59eeebe1d000871ebe2711
sanyinchen <sanyinchen@baidu.com>
2020-07-02 11:21:45 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/nasm-hello_asm.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM_NASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM_NASM
+  "/home/sanyinchen/CLionProjects/hello-asm/src/asm/hello.asm" "/home/sanyinchen/CLionProjects/hello-asm/cmake-build-debug/CMakeFiles/nasm-hello_asm.dir/src/asm/hello.asm.o"
+  )
+set(CMAKE_ASM_NASM_COMPILER_ID "NASM")
+
+# The include file search paths:
+set(CMAKE_ASM_NASM_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/nasm-hello_asm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nasm-hello_asm.dir/src/asm/hello.asm.o"
+  "nasm-hello_asm"
+  "nasm-hello_asm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM_NASM)
+  include(CMakeFiles/nasm-hello_asm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
